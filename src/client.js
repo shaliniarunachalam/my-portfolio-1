d741247b346fb52c46bf0ac502f6923e81d0bde1
shaliniarunachalam <shalinieee97@gmail.com>
2021-08-18 12:03:14 +0530

    initial commit

---
+++ src/client.js
@@ -0,0 +1,9 @@
+import SanityClientConstructor from "@sanity/client"
+import sanityClient from "@sanity/client"
+
+export default SanityClient(
+    {
+        projectId: "815pf4pb",
+        dataset: "production"
+    }
+)
